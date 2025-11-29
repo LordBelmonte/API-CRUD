229470d8b563804b352080b29c1816c8355703a7
Higor Dias <128336683+Raposa1996@users.noreply.github.com>
2025-11-29 12:16:05 -0300

    ajuste config para Servidores diferentes

--- config/config.php
+++ config/config.php
@@ -3,5 +3,20 @@
     const dbEndereco = "localhost";
     const dbNome = "ongdogs";
     const dbUsuario = "root";
-    const dbSenha = "SI1234";
+    const dbSenha = "";
+
+    try {
+        $conexao = new PDO(
+            dbDrive . ":host=" . dbEndereco . ";dbname=" . dbNome . ";charset=utf8",
+            dbUsuario,
+            dbSenha
+        );
+
+        // Habilita o modo de erros
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        echo "✔ Conexão bem-sucedida!";
+    } catch (PDOException $e) {
+        echo "❌ Erro ao conectar: " . $e->getMessage();
+    }
 ?>
